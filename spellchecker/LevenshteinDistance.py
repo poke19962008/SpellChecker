305a7d945698f87685ed7d0858a8dcbbe8760a54
Sayan Das <poke19962008@gmail.com>
2016-02-28 13:43:57 +0530

    added train method

--- spellchecker/LevenshteinDistance.py
+++ spellchecker/LevenshteinDistance.py
@@ -1,5 +1,1 @@
-def compute(start, end):
-    # lenStart = len(start)
-    # lenEnd = len(end)
-    #
-    # dp = [[0 for x in range(lenStart)] for x in range(lenEnd)]
+def getInitialState(lenStartW, lenEndW):
--- spellchecker/main.py
+++ spellchecker/main.py
@@ -1,5 +1,35 @@
 import LevenshteinDistance as LD
+import re, pymongo
 
 class SpellChecker:
     def __init__(self):
-        # LD.compute('hello', 'start')
+        LD.compute('hello', 'start')
+
+    def train(self):
+        corpus = open('spellchecker/corpus.txt').read()
+
+        try:
+            con = pymongo.Connection(host="mongodb://localhost:27017/spellchecker", port=27017)
+            print "Connected to MongoDB"
+        except:
+            print "[ERROR] Cannot connect to MongoDB"
+
+        db = con['spellchecker']
+        counter = 0
+        print "Started inserting in Mongo"
+        for row in corpus.split('\n'):
+            data = {}
+            row = row.split('\t')
+
+            data['rank'] = row[0]
+            data['word'] = re.sub(r"\s{3}", "", row[1])
+            data['frequency'] = row[3]
+            data['dispersion'] = row[4]
+
+            try:
+                db.dictionary.insert(data)
+                counter = counter + 1
+            except:
+                print "[ERROR] Cannot insert " + data['word']
+
+        print "[SUCCESS] Inserted " + str(counter) + " of " + str(len(corpus.split('\n')))
